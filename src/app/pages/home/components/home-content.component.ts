08697fe614166d45c1226850d8a59156285948a6
Lucas Lagnes <lucaslagnes@MacBook-Air-de-Lucas.local>
2023-10-19 14:07:08 +0200

    [Modify] Test

---
+++ src/app/pages/home/components/home-content.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-content',
+  templateUrl: './home-content.component.html',
+  styleUrls: ['./home-content.component.css'],
+})
+export class HomeContentComponent { }
---
+++ src/app/pages/workspace/workspace.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+import { WorkspaceComponent } from './workspace.component';
+
+export const workspaceRoutes: Routes = [
+  {
+    path: '',
+    component: WorkspaceComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('../home/home.module').then((m) => m.HomeModule),
+      },
+      { path: '**', redirectTo: 'error' },
+    ],
+  },
+];
