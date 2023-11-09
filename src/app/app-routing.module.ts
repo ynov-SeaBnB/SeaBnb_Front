fbff01fe918fba8800a5d297e67c26d5a2bde595
CES Grégory <gregory.ces@soprasteria.com>
2023-11-09 09:23:51 +0100

    New architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {
-    path: "",
-    loadChildren: () =>
-      import("./pages/workspace/workspace.module").then(
-        (m) => m.WorkspaceModule,
-      ),
-  },
+  {path: '', component:HomeComponent},
+  {path: 'home', component:HomeComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/pages/home/components/home-header.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "home-header",
-  template: ``,
-})
-export class HomeHeaderComponent {}
