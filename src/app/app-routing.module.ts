f2194fef07d15ce5453cf1ed0fe48dbaf3c753b6
CES Grégory <gregory.ces@soprasteria.com>
2023-11-09 10:03:16 +0100

    GC : working on routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProfilComponent } from './profil/profil.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'home', component:HomeComponent},
+  {path: 'profil', component:ProfilComponent},
 ];
 
 @NgModule({
