bfed424db8fe25b0b87c93e7cf3271bd24d16fc0
CES Grégory <gregory.ces@soprasteria.com>
2023-11-28 09:28:50 +0100

    GC: Install Heroicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfilComponent } from './profil/profil.component';
 import { DescriptionComponent } from './description/description.component';
 import { OwnerComponent } from './owner/owner.component';
+import { NgIconsModule } from '@ng-icons/core';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { OwnerComponent } from './owner/owner.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgIconsModule.withIcons({heroUsers}),
   ],
   providers: [],
   bootstrap: [AppComponent]
