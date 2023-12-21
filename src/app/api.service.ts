75c433ff28b088c2d7549d6d095b305e27502d50
Nwqr <Nwqr@users.noreply.github.com>
2023-12-21 14:13:26 +0000

    Prettified Code!

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   private API_URL = 'http://yourbackend.com/api';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgIconsModule } from '@ng-icons/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { heroBars3, heroUserCircle, heroUser, heroLockClosed, heroEnvelope, heroLanguage, heroCamera, heroChevronRight, heroArrowUturnLeft } from '@ng-icons/heroicons/outline';
+import {
+  heroBars3,
+  heroUserCircle,
+  heroUser,
+  heroLockClosed,
+  heroEnvelope,
+  heroLanguage,
+  heroCamera,
+  heroChevronRight,
+  heroArrowUturnLeft,
+} from '@ng-icons/heroicons/outline';
 import { heroStarSolid } from '@ng-icons/heroicons/solid';
 import { BoatsComponent } from './boats/boats.component';
 import { DescriptionComponent } from './description/description.component';
@@ -34,9 +44,20 @@ import { OwnerComponent } from './owner/owner.component';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgIconsModule.withIcons({ heroUser, heroEnvelope, heroLockClosed, heroBars3, heroUserCircle, heroStarSolid, heroLanguage, heroCamera, heroChevronRight, heroArrowUturnLeft }),
+    NgIconsModule.withIcons({
+      heroUser,
+      heroEnvelope,
+      heroLockClosed,
+      heroBars3,
+      heroUserCircle,
+      heroStarSolid,
+      heroLanguage,
+      heroCamera,
+      heroChevronRight,
+      heroArrowUturnLeft,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
